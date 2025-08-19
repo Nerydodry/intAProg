8933ce358d14af66dc8c879009a7f13b0f6116a4
Nerydodry <luannery29@gmail.com>
2025-08-19 08:53:19 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,6 @@
+#num = int (input ('Digite um numero: '))
+#x = num*1 , num*2 , num*3 , num*4 , num*5 , num*6 , num*7 , num*8 , num*9 , num*10
+#for n in range(1):
+   #print('A tabuada desse número é: ', x)
+for l in range(10):
+    print(l)
