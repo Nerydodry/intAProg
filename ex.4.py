033325fc8ad2fc67a71a9b54e11978350d6d6817
Nerydodry <luannery29@gmail.com>
2025-08-26 08:46:39 -0300

    Add files via upload

---
+++ ex.4.py
@@ -0,0 +1,5 @@
+soma=0
+x=int(input('Digite um numero: '))
+for i in range(1,x+1):
+    soma+=i
+print ('A soma desses números é: ', soma)    
---
+++ ex.5.py
@@ -0,0 +1,5 @@
+n_jogador = input('Digite o número de jogadores: ')
+m_altura = float(input('Digite a altura dos jogadores: '))
+for i in range(1,n_jogador+1):
+    m_altura + n_jogaor /2
+print('A média de altura dos jogaodres é de: ')
