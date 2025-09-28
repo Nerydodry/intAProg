14b35614e90d46166bb34a1e247d3744c9e5bc3e
Nerydodry <luannery29@gmail.com>
2025-09-28 12:12:16 -0300

    Delete ex.8.py

--- ex.8.py
+++
@@ -1,8 +0,0 @@
-thanos=1.50
-miranha=1.10
-for i in range (1000):
-    if miranha > thanos:
-        break
-    thanos+=0.02
-    miranha+=0.03
-print('Vai demorar ',i, 'anos para que o Homem Aranha seja maio que o Thanos')
